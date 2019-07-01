e583a623f58f829964209a0137fd961dc98218ad
Cody <crose992@gmail.com>
2019-07-01 17:36:15 -0400

    Refactor Form to map over array of inputs to render TextInputs

--- app/components/Form.js
+++ app/components/Form.js
@@ -3,104 +3,86 @@ import PropTypes from 'prop-types'
 import TextInput from './TextInput'
 
 const Form = props => {
+	const inputs = [
+		{
+			label: 'Portrait URL',
+			name: 'portrait'
+		},
+		{
+			label: 'Logo URL',
+			name: 'logo'
+		},
+		{
+			label: 'First name',
+			name: 'firstName'
+		},
+		{
+			label: 'Last name',
+			name: 'lastName'
+		},
+		{
+			label: 'Job title',
+			name: 'jobTitle'
+		},
+		{
+			label: 'Department',
+			name: 'department'
+		},
+		{
+			label: 'Company name',
+			name: 'companyName'
+		},
+		{
+			label: 'Office phone',
+			name: 'officePhone'
+		},
+		{
+			label: 'Mobile phone',
+			name: 'mobilePhone'
+		},
+		{
+			label: 'Email address',
+			name: 'emailAddress'
+		},
+		{
+			label: 'Website URL',
+			name: 'websiteUrl'
+		},
+		{
+			label: 'Address',
+			name: 'address'
+		},
+		{
+			label: 'Facebook',
+			name: 'socialFacebook'
+		},
+		{
+			label: 'Twitter',
+			name: 'socialTwitter'
+		},
+		{
+			label: 'LinkedIn',
+			name: 'socialLinkedin'
+		},
+		{
+			label: 'Instagram',
+			name: 'socialInstagram'
+		},
+	]
+
 	return(
 		<form className="input-group">
-			<TextInput
-				label="Portrait URL"
-				name="portrait"
-				value={ props.portrait }
-				onBlur={ props.onInputChange }
-			/>
-			<TextInput
-				label="Logo URL"
-				name="logo"
-				value={ props.logo }
-				onBlur={ props.onInputChange }
-			/>
-			<TextInput
-				label="First name"
-				name="firstName"
-				value={ props.firstName }
-				onBlur={ props.onInputChange }
-			/>
-			<TextInput
-				label="Last name"
-				name="lastName"
-				value={ props.lastName }
-				onBlur={ props.onInputChange }
-			/>
-			<TextInput
-				label="Job title"
-				name="jobTitle"
-				value={ props.jobTitle }
-				onBlur={ props.onInputChange }
-			/>
-			<TextInput
-				label="Department"
-				name="department"
-				value={ props.department }
-				onBlur={ props.onInputChange }
-			/>
-			<TextInput
-				label="Company name"
-				name="companyName"
-				value={ props.companyName }
-				onBlur={ props.onInputChange }
-			/>
-			<TextInput
-				label="Office phone"
-				name="officePhone"
-				value={ props.officePhone }
-				onBlur={ props.onInputChange }
-			/>
-			<TextInput
-				label="Mobile phone"
-				name="mobilePhone"
-				value={ props.mobilePhone }
-				onBlur={ props.onInputChange }
-			/>
-			<TextInput
-				label="Email address"
-				name="emailAddress"
-				value={ props.emailAddress }
-				onBlur={ props.onInputChange }
-			/>
-			<TextInput
-				label="Website URL"
-				name="websiteUrl"
-				value={ props.websiteUrl }
-				onBlur={ props.onInputChange }
-			/>
-			<TextInput
-				label="Address"
-				name="address"
-				value={ props.address }
-				onBlur={ props.onInputChange }
-			/>
-			<TextInput
-				label="Facebook"
-				name="socialFacebook"
-				value={ props.socialFacebook }
-				onBlur={ props.onInputChange }
-			/>
-			<TextInput
-				label="Twitter"
-				name="socialTwitter"
-				value={ props.socialTwitter }
-				onBlur={ props.onInputChange }
-			/>
-			<TextInput
-				label="LinkedIn"
-				name="socialLinkedin"
-				value={ props.socialLinkedin }
-				onBlur={ props.onInputChange }
-			/>
-			<TextInput
-				label="Instagram"
-				name="socialInstagram"
-				value={ props.socialInstagram }
-				onBlur={ props.onInputChange }
-			/>
+			{ inputs.map(input => {
+				return(
+					<TextInput
+						label={input.label}
+						name={input.name}
+						value={props[input.name]}
+						onBlur={props.onInputChange}
+						key={input.name}
+					/>
+				)
+			}) }
 		</form>
 	)
 }
