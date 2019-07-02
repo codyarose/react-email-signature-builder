27bf6db914180627ec3f61d54602c8e2a75e3696
Cody Rose <codyrose@GLaDOS.local>
2019-07-02 11:29:31 -0400

    Refactor Accordion to styled-component

--- app/components/Accordion.js
+++ app/components/Accordion.js
@@ -1,16 +1,107 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Accordion = props => {
 	return(
-		<article className="accordion">
-			<input className="accordion__checkbox" type="checkbox" defaultChecked />
-			<span className="accordion__arrows"></span>
-			<h3 className="accordion__title">{ props.title }</h3>
-			<section className="accordion__content">
-				{ props.children }
-			</section>
-		</article>
+		<StyledAccordion>
+			<Input type="checkbox" defaultChecked />
+			<Arrows />
+			<Title>{ props.title }</Title>
+			<Content>{ props.children }</Content>
+		</StyledAccordion>
 	)
 }
 
 export default Accordion
+
+const StyledAccordion = styled.article`
+	width: 100%;
+	position: relative;
+	animation: flipdown .5s ease both;
+	margin-bottom: 1.5rem;
+
+	@keyframes flipdown {
+	0% {
+		opacity: 0;
+		transform-origin: top center;
+		transform: rotateX(-90deg);
+	}
+	5% {
+		opacity: 1;
+	}
+	80% {
+		transform: rotateX(8deg);
+	}
+	83% {
+		transform: rotateX(6deg);
+	}
+	100% {
+		transform-origin: top center;
+		transform: rotateX(0deg);
+	}
+}
+`
+
+const Input = styled.input`
+	position: absolute;
+	cursor: pointer;
+	width: 100%;
+	height: 100%;
+	z-index: 1;
+	opacity: 0;
+	margin: 0;
+	&:checked {
+		& ~ section {
+			margin-top: 0;
+			max-height: 0;
+			opacity: 0;
+			transform: translate(0, 50%);
+		}
+		& ~ span {
+			&::before {
+				transform: translate(100%, 0) rotate(45deg);
+			}
+			&::after {
+				transform: translate(-100%, 0) rotate(-45deg);
+			}
+		}
+	}
+`
+
+const Arrows = styled.span`
+	position: absolute;
+	margin-top: 12px;
+	right: 0;
+	&::before,
+	&::after {
+		content: '';
+		position: absolute;
+		background-color: #fff;
+		width: 2px;
+		height: 7px;
+		transition: transform .1s ease-out;
+	}
+	&::before {
+		transform: translate(-100%, 0) rotate(45deg)
+	}
+	&::after {
+		transform: translate(100%, 0) rotate(-45deg)
+	}
+`
+
+const Title = styled.h3`
+	display: block;
+	margin: 0;
+	cursor: pointer;
+`
+
+const Content = styled.section`
+	position: relative;
+	overflow: hidden;
+	opacity: 1;
+	transition: all .3s ease-in-out;
+	transform: translate(0, 0);
+	padding-top: 1rem;
+	z-index: 2;
+	max-height: 1000px;
+`
