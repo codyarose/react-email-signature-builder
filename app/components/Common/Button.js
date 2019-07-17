ed1031b4a872a87c5e36ae99a05b918b6c905902
codyarose <crose992@gmail.com>
2019-07-17 13:16:42 -0400

    Add secondary version of Button component

--- app/components/Common/Button.js
+++ app/components/Common/Button.js
@@ -6,6 +6,10 @@ export const Button = ({ onClick, children }) => (
 	<StyledButton onClick={onClick}>{children}</StyledButton>
 )
 
+export const ButtonSecondary = ({ onClick, children }) => (
+	<StyledButton secondary onClick={onClick}>{children}</StyledButton>
+)
+
 Button.propTypes = {
 	onClick: PropTypes.func,
 	children: PropTypes.string,
@@ -13,16 +17,20 @@ Button.propTypes = {
 
 const StyledButton = styled.button`
 	outline: none;
-	border: none;
-	background-color: #504a65;
-	color: #fff;
+	border: 1px solid transparent;
+	background-color: ${props => props.secondary ? '#fff' : '#504a65'};
+	color: ${props => props.secondary ? '#504a65' : '#fff'};
 	font-family: inherit;
 	padding: 0.5rem 1rem;
 	margin-bottom: 1rem;
 	cursor: pointer;
-	transition: background-color 0.1s ease-out;
+	transition:
+		color 0.1s ease-out,
+		border-color 0.1s ease-out,
+		background-color 0.1s ease-out;
 	&:hover {
 		background-color: #696284;
+		${props => props.secondary ? 'color: #fff' : null}
 	}
 	&:active {
 		background-color: #504a65;
