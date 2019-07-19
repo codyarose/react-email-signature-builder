006e89e110581b3835d518039bcdb81462bfcc8a
codyarose <crose992@gmail.com>
2019-07-19 10:56:19 -0400

    Updates

---
+++ precache-manifest.84982dea0c0e1fb561660133b780a7f2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df93e866c29c8ca27aa956cec7ec8b8e",
+    "url": "/react-email-signature-builder/index.html"
+  },
+  {
+    "revision": "fddd711343c80a97625c",
+    "url": "/react-email-signature-builder/static/js/2.9fcac327.chunk.js"
+  },
+  {
+    "revision": "f8de8578a2b2719a6a76",
+    "url": "/react-email-signature-builder/static/js/main.c9cbfa4c.chunk.js"
+  },
+  {
+    "revision": "2b836f3b042f909077e7",
+    "url": "/react-email-signature-builder/static/js/runtime~main.5dcda161.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-email-signature-builder/precache-manifest.84982dea0c0e1fb561660133b780a7f2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-email-signature-builder/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
